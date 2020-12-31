8d9a965f1a6fa02ae42c4487f3d8f09c4e580b43
davidjosephind <davidjosephind@gmail.com>
2020-12-31 12:56:02 +0530

    Add ejs

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const ejs = require("ejs");
 
 const app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", function (req, res) {
-  res.send("Hello there, David says hi!");
+  res.render("home.ejs");
 });
 
 app.listen(process.env.PORT || 3000, function () {
