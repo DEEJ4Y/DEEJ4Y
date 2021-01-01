891f35c2f8f02ee3b94b68027f017a31fefaff27
davidjosephind <davidjosephind@gmail.com>
2021-01-01 19:38:21 +0530

    Add home and contact page

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const ejs = require("ejs");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function (req, res) {
-  res.render("home.ejs");
-});
+app
+  .route("/")
+  .get((req, res) => {
+    res.render("home.ejs");
+  })
+  .post((req, res) => {
+    const requestedPage = req.body.requestedPage;
+    if (requestedPage == "projects") {
+      res.render("projects.ejs");
+    } else if (requestedPage == "contact") {
+      res.render("contact.ejs");
+    } else if (requestedPage == "home") {
+      res.render("home.ejs");
+    } else {
+      res.render("error.ejs");
+    }
+  });
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server started on port 3000");
 });
