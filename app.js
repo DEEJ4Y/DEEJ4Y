2bb38a3578f092753e4d46dd5e6a456a6a90a770
davidjosephind <davidjosephind@gmail.com>
2021-01-01 22:28:24 +0530

    Add mongoose, dotenv. Connect to mongoAtlas server

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
+require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(
+  "mongodb+srv://admin-david:" +
+    process.env.MONGO_ATLAS_PASSWORD +
+    "@cluster0.u7o6u.mongodb.net/ProjectsDB?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+const projectSchema = new mongoose.Schema({
+  name: String,
+  info: String,
+  url: String,
+  type: String,
+});
+
+const Project = mongoose.model("Project", projectSchema);
 
 app
   .route("/")
